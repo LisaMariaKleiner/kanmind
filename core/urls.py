f10f9ae53aad532c5ae56cef3b55c1c9b8550fdd
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-09-18 17:32:01 +0200

    feat: Aktivieren der URL-Routing für die Aufgaben-API in der Haupt-URL-Konfiguration

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication_app.api.urls')),
     path('api/boards/', include('boards_app.api.urls')),
-    # path('api/tasks/', include('tasks_app.api.urls')),
+    path('api/tasks/', include('tasks_app.api.urls')),
 ]
